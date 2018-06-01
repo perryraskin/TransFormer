c801e21861636ad4bc873fc6c488eb3371f7b493
Perry Raskin <perryraskin@gmail.com>
2018-05-31 23:37:28 -0400

    initial program: parses CSV to main CSV

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# TransFormer
-An app that makes managing personal finances, actually personal
+## TransFormer: Transaction Formatter
+![](rm_image.jpg)
+
+**Purpose:** To make managing personal finances actually personal. It should be super simple, without needing to worry about your bank or credit card info getting stolen by very probable cyber attacks (on Mint, for example).
+
+**Details:** Right now it's meant to leave you with a basic CSV file that includes all the basic data you would need to plot financial data. It takes data from Chase or CapitalOne transaction CSV downloads, and loads it into a usable CSV. To use with *Matplotlib* or anything you may use to plot data from a CSV. This assumes you can plot using Python or another language.
+
+**Checklist:**
+
+* :heavy_check_mark: Chase CSV support
+* :heavy_check_mark: CapitalOne CSV support
+* :heavy_check_mark: Converts to specified CSV format: **[Date, Amount, Description, Category]**
+* :heavy_check_mark: Adds positive float number for purchases, negative float numbers for refunds
+
+**To Do:**
+
+* Integrate banks API for easy CSV transaction access
+* Create a new CSV for every import, and only grab the new data
---
+++ TransFormer.py
@@ -0,0 +1,103 @@
+import csv
+
+def isFloat(string):
+    try:
+        float(string)
+        return True
+    except ValueError:
+        return False
+
+def isChase(file):
+    first = 'Type'
+
+    with open(file, 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if first == row[0]:
+                return True
+    return False
+
+def parseChaseData(file):
+    with open(file, 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)
+        refund = 0
+        amount = 0
+        for row in reader:
+            date = row[1]
+            if float(row[4]) < 0:
+                amount = float(row[4]) + (abs(float(row[4]))*2)
+                refund = 0
+            else:
+                refund = 0 - float(row[4])
+                amount = 0
+            desc = row[3]
+            ctg = row[5]
+            addTransaction(date, amount, desc, ctg, refund)
+
+def isCapitalOne(file):
+    first = 'Transaction Date'
+
+    with open(file, 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            if first == row[0]:
+                return True
+    return False
+
+def parseCapitalOneData(file):
+    with open(file, 'rt') as f:
+        reader = csv.reader(f, delimiter=',')
+        next(reader)
+        refund = 0
+        amount = 0
+        for row in reader:
+            date = row[0]
+            if isFloat(row[5]):
+                amount = float(row[5])
+                refund = 0
+            else:
+                refund = float(row[6])
+                refund = refund - (2*refund)
+                amount = 0
+                if desc.find('CAPITAL ONE MOBILE PYMT'):
+                    refund = 0
+            desc = row[3]
+            ctg = row[4]
+                
+            addTransaction(date, amount, desc, ctg, refund)
+
+# def isDiscover()
+
+def addTransaction(date, amount, desc, ctg, refund):
+    with open('all_transactions_test.csv', 'a') as newFile:
+        reader = csv.reader(newFile, delimiter=',')
+        newFileWriter = csv.writer(newFile)
+        if amount > 0:
+            newFileWriter.writerow([date, str(amount), desc, ctg])
+            print ("New transaction added!")
+        else:
+            newFileWriter.writerow([date, refund, desc, ctg])
+            print ("New refund added!")
+
+def main():
+    """ ASK USER FOR CSV TO IMPORT """
+    count = 0
+    print ("Enter CSV filename: ", end='')
+    file = input()
+
+    try:
+        
+      if isChase(file):
+        parseChaseData(file)
+      elif isCapitalOne(file):
+        parseCapitalOneData(file)
+
+    except Exception as e:
+        raise e
+        print ("An error has occured. Please try again.")
+
+    return 0
+
+if __name__ == "__main__":
+    main()
